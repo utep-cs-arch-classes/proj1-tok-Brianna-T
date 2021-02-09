e74a442b4bae5d840aa07de3b76ac6a4f20f3297
Brianna T <btovar@miners.utep.edu>
2021-02-08 19:41:59 -0700

    test

---
+++ src/Milestone1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  char user[20]://length of what the user's string can be
+    //DONT USE GETS and PUTS
+    printf("Type anything, $");
+  for(char user;(user=getchar())&&i!>20;i++){
+    putchar(i);
+  }
+  return 0; //closing main, exits
+}
